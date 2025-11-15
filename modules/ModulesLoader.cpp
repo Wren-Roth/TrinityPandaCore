edf93bcd962927394a0b0f2b01f82a62f398e848
wren <korrarainbows@gmail.com>
2025-11-15 13:19:29 -0500

    change, idk

--- modules/ModulesLoader.cpp
+++ modules/ModulesLoader.cpp
@@ -3,10 +3,10 @@
 #include <string>
 
 // Includes list
-void AddSC_solocraft_system();
 void AddSC_mod_items();
 void AddSC_mod_leech();
 void AddSC_mod_example();
+void AddSC_solocraft_system();
 
 
 #include "ModulesLoader.h"
@@ -15,9 +15,9 @@ void AddSC_mod_example();
 void AddModulesScripts()
 {
     // Modules
-    AddSC_solocraft_system();
-    AddSC_mod_items();
+        AddSC_mod_items();
     AddSC_mod_leech();
     AddSC_mod_example();
+    AddSC_solocraft_system();
 
 }
